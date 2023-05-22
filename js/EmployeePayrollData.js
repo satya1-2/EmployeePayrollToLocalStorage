d8b2cdd977e3a8084032145bdcedc164c70a43a5
satya <satya143@gmail.com>
2023-05-22 18:35:01 +0530

    Add:Ability to create Employee Payroll object to save

--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -99,3 +99,69 @@ window.addEventListener('DOMContentLoaded',(event) => {
    });
 });
 
+//..........UC3 AND UC4
+/**
+* 
+* Ability to save the Employee Payroll Object to Local Storage.
+* @returns it returns the data in EmployeepayrollData.
+*/
+
+const save = () => {
+   try {
+       let employeePayrollData = createEmployeePayroll();
+       createAndUpdateStorage(employeePayrollData);
+   } catch (e) {
+       return;
+   }
+}
+
+function createAndUpdateStorage(employeePayrollData){
+   let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+
+   if(employeePayrollList != undefined){
+       employeePayrollList.push(employeePayrollData);
+   }else{
+       employeePayrollList = [employeePayrollData];
+   }
+   alert(employeePayrollList.toString());
+   localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList))
+}
+
+const createEmployeePayroll = () => {
+   let employeePayrollData = new EmployeePayrollData();
+   try {
+       employeePayrollData.name = getInputValueById('#name');
+   } catch (e) {
+       setTextValue('.text-error',e);
+       throw e;
+   }
+
+   employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+   employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+   employeePayrollData.department = getSelectedValues('[name=department]');
+   employeePayrollData.salary = getInputValueById('#salary');
+   employeePayrollData.note = getInputValueById('#notes');
+
+   let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+getInputValueById('#year');
+   employeePayrollData.date = Date.parse(date);
+   alert(employeePayrollData.toString());
+   return employeePayrollData;
+}
+const getSelectedValues = (propertyValue) => {
+       let allItems = document.querySelectorAll(propertyValue);
+       let setItems = [];
+       allItems.forEach(item => {
+           if(item.checked) setItems.push(item.value);
+       });
+       return setItems;
+}
+
+const getInputValueById = (id) => {
+   let value = document.querySelector(id).value;
+   return value;
+}
+
+const getInputElementValue = (id) => {
+   let value = document.getElementById(id).value;
+   return value;
+}
